d88b0517f1ecf7c8ff6c01810f418c0571f2f4fd
Kaoru NAKAMURA <kaorun55@gmail.com>
2016-05-19 10:59:42 +0900

    サンプルアプリを作成

---
+++ SocketSample/Assets/Scripts/ListenerSocket.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Collections.Generic;
+
+public class ListenerSocket {
+#if false
+    Socket listener;
+    SocketAsyncEventArgs e = new SocketAsyncEventArgs();
+
+    public ListenerSocket()
+    {
+        e.Completed += OnAcceptCompleted;
+    }
+
+    public void Listen( int port )
+    {
+        listener = new Socket( AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp );
+        listener.Bind( new IPEndPoint( IPAddress.Any, port ) );
+        listener.Listen( 10 );
+
+        listener.AcceptAsync(e);
+    }
+
+    private void OnAcceptCompleted( object sender, SocketAsyncEventArgs e )
+    {
+        Debug.Log( "OnAcceptCompleted" );
+    }
+#else
+    Socket listener;
+
+    public ListenerSocket()
+    {
+    }
+
+    public void Listen(int port)
+    {
+        listener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        listener.Bind(new IPEndPoint(IPAddress.Any, port));
+        listener.Listen(10);
+
+        listener.BeginAccept((ar)=> {
+            Debug.Log("Accept");
+        }, null);
+    }
+#endif
+}
---
+++ SocketSample/Assets/Scripts/ServerSocket.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ServerSocket : MonoBehaviour {
+    ListenerSocket listener = new ListenerSocket();
+	// Use this for initialization
+	void Start () {
+        listener.Listen(55555);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
